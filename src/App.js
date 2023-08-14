df74f08c332141cd94aaec8d4dea3cbc806405ba
codebyharshit <harshitagrawal048@gmail.com>
2023-08-14 22:57:33 +0530

    Navbar component added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProductList from "./features/product/ProductList";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import Navbar from "../features/navbar/Navbar";
+import ProductList from "../features/product/ProductList";
+
+
+function Home() {
+  return (
+    <div>
+      <Navbar>
+        <ProductList>
+
+        </ProductList>
+      </Navbar>
+    </div>
+  );
+}
+
+
+export default Home;
